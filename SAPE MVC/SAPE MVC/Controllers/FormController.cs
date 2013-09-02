035d35d65c3e096ae0931af91995a3733429da38
Sebastian Ramirez <sramirez10r@gmail.com>
2013-09-02 00:03:35 -0600

    Base del form RegistroEmpresas

---
+++ SAPE MVC/SAPE MVC/Controllers/FormController.cs
@@ -0,0 +1,37 @@
+﻿using SAPE_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SAPE_MVC.Controllers
+{
+    public class FormController : Controller
+    {
+        //
+        // GET: /Form/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RegistroEmpresas()
+        {
+            SAPEEntities database = new SAPEEntities();
+            ViewBag.Ciudades = database.Provincia.OrderBy(ciudad=> ciudad.Nombre);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistroEmpresas(string nombreEmpresa, string practicantes, int cantPracticantes, string ciudad, string nombreContacto, string papellidoContacto, string sapellidoContacto, string telContacto)
+        {
+            SAPEEntities entities = new SAPEEntities();
+
+
+            return View();
+        }
+    }
+}
---
+++ db_fill.sql
@@ -0,0 +1,10 @@
+USE [SAPE] 
+INSERT INTO [dbo].[Pais] ([Nombre])VALUES('Costa Rica')
+INSERT INTO [dbo].[Provincia] ([Nombre], [FK_Pais])VALUES('San Jose', 1)
+INSERT INTO [dbo].[Provincia] ([Nombre], [FK_Pais])VALUES('Heredia', 1) 
+INSERT INTO [dbo].[Provincia] ([Nombre], [FK_Pais])VALUES('Guanacaste', 1) 
+INSERT INTO [dbo].[Provincia] ([Nombre], [FK_Pais])VALUES('Alajuela', 1)
+INSERT INTO [dbo].[Provincia] ([Nombre], [FK_Pais])VALUES('Limon', 1) 
+INSERT INTO [dbo].[Provincia] ([Nombre], [FK_Pais])VALUES('Cartago', 1) 
+INSERT INTO [dbo].[Provincia] ([Nombre], [FK_Pais])VALUES('Puntarenas', 1) 
+GO
