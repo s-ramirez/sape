cf382351fe5f1db823b61e21a40438737e1bdea6
Rodrigo Ugalde Valerio <ruvajo@gmail.com>
2013-10-26 17:10:30 -0600

    Formulario de Registro Empresas

--- SAPE MVC/SAPE MVC/Models/SAPE_Entities.Designer.cs
+++ SAPE MVC/SAPE MVC/Models/SAPE_Entities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Sebastian Ramirez\Documents\SAPE\SAPE MVC\SAPE MVC\Models\SAPE_Entities.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\SAPE\Desktop\Proyecto\SAPE MVC\SAPE MVC\Models\SAPE_Entities.edmx'. 
+// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
+// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
+// el modelo en el diseñador.
